af6f279db98ccd227a7e5a2b63a899e5877e3997
elnazsharif <123261273+elnazsharif@users.noreply.github.com>
2025-11-06 00:24:16 +0000

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ os.makedirs("weights", exist_ok=True)
 # 🔹 Replace these with your actual Google Drive direct download links
 model_links = {
     "acne_best.pt": "https://drive.google.com/uc?export=download&id=1ob9BO_AsvXL1rBEDCdryxjnWLu4KRrHz",
-    "darkcircle_best.pt": "https://drive.google.com/uc?export=download&id=1o9i07SIm1lXCOc_C7Hk_rREM6YaX7aWH",
+    "pigmentation_best.pt": "https://drive.google.com/uc?export=download&id=1hzkesH6aF0FSKgfX-BpmaJ61X8pFDNpT",
 
    
 }
@@ -54,7 +54,7 @@ for filename, url in model_links.items():
 print("🧠 Loading YOLO models into memory...")
 MODELS = {
     "acne": YOLO("weights/acne_best.pt"),
-    "darkcircle": YOLO("weights/darkcircle_best.pt"),
+    "pigmentation": YOLO("weights/pigmentation_best.pt"),
     
 }
 print("✅ All models loaded successfully.")
